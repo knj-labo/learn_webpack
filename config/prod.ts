67429c9b31f80f732fb201676a2b04d0e6e3fdb5
jp-knj <kenji.tomita1996@gmail.com>
2022-04-10 17:00:48 +0900

    :sparkles: install terser webpack plugin

--- config/prod.ts
+++ config/prod.ts
@@ -1,6 +1,7 @@
 import * as webpack from 'webpack'
 import { merge } from 'webpack-merge'
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+import TerserPlugin  from 'terser-webpack-plugin';
 
 import { baseConfig } from "./base.js";
 
@@ -9,6 +10,32 @@ const prodConfig: webpack.Configuration = merge(baseConfig, {
     plugins: [
         new BundleAnalyzerPlugin(),
     ],
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                parallel: 4,
+                terserOptions: {
+                    parse: {
+                        ecma: 2018,
+                    },
+                    compress: {
+                        ecma: 5,
+                        warnings: false,
+                        comparisons: false,
+                        inline: 2,
+                    },
+                    mangle: {
+                        safari10: true,
+                    },
+                    output: {
+                        ecma: 5,
+                        comments: false,
+                        ascii_only: true,
+                    },
+                },
+            }),
+        ],
+    },
 });
 
 export default prodConfig;
